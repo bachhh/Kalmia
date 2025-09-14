6f1996b9d22db4a4ad8bca76cf75d7751d0cd309
Do Bach <bach@h2corporation.jp>
2025-09-14 16:57:28 +0900

    remove asciiart

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,16 +8,6 @@ import (
 
 const Version = "0.2.0"
 
-func AsciiArt() {
-	fmt.Println(" _   __      _           _")
-	fmt.Println("| | / /     | |         (_)")
-	fmt.Println("| |/ /  __ _| |_ __ ___  _  __ _")
-	fmt.Println("|    \\ / _` | | '_ ` _ \\| |/ _` |")
-	fmt.Println("| |\\  \\ (_| | | | | | | | | (_| |")
-	fmt.Println("\\_| \\_/\\__,_|_|_| |_| |_|_|\\__,_|")
-	fmt.Printf("\t\t            v%s\n", Version)
-}
-
 func ParseFlags() string {
 	configPath := flag.String("config", "./config.json", "path to config file")
 	help := flag.Bool("help", false, "print help and exit")
